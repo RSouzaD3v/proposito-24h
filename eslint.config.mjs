ca988ceabe9e45b9a9ef31f41f00d970dc76c772
hulkheineken <hulkheineken@gmail.com>
2025-08-10 16:31:15 -0300

    disabled no-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // desativa erro de any
+      "@typescript-eslint/no-unused-vars": "off", // desativa erro de variável não usada
+      "@next/next/no-img-element": "off", // desativa aviso do <img>
+    },
+  },
 ];
 
 export default eslintConfig;
