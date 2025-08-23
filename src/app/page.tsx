a6a61484650598751359eeaebc14f9db02a59074
RSouzaD3v <rafaelf.souzan02@gmail.com>
2025-08-23 14:49:28 -0300

    lading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,16 @@ export default function Home() {
     <section>
       <Header />
 
-      <section className='h-screen max-h-[650px]'>
-        <div className='absolute top-0 left-0 -z-10 w-full min-h-screen flex items-center justify-center flex-col gap-4'>
-            <video autoPlay={true} muted loop className='absolute w-full h-full object-cover'>
-              <source src="/video/man-woman.mp4" type="video/mp4" />
-            </video>
-            <div className='bg-black absolute top-0 left-0 w-full h-full opacity-70' />
-            <div className='relative z-10 flex flex-col items-center justify-center'>
-              <h1 className='text-white md:text-4xl text-2xl font-bold'>Seja bem vindo ao <b>propósito24h</b></h1>
-              <p className='text-white md:text-lg'>Onde você vai criar mais conexão com Deus!</p>
-              <Link href="/register-writer" className='mt-4 px-10 py-2 bg-amber-400 text-black font-bold rounded-md'>Começar</Link>
-            </div>
-        </div>
+      <section className='absolute top-0 left-0 -z-10 w-full min-h-screen flex items-center justify-center flex-col gap-4'>
+          <video autoPlay={true} muted loop className='absolute w-full h-full object-cover'>
+            <source src="/video/man-woman.mp4" type="video/mp4" />
+          </video>
+          <div className='bg-black absolute top-0 left-0 w-full h-full opacity-70' />
+          <div className='z-10 flex flex-col items-center justify-center'>
+            <h1 className='text-white md:text-4xl text-2xl font-bold'>Seja bem vindo ao <b>propósito24h</b></h1>
+            <p className='text-white md:text-lg'>Onde você vai criar mais conexão com Deus!</p>
+            <Link href="/register-writer" className='mt-4 px-10 py-2 bg-amber-400 text-black font-bold rounded-md'>Começar</Link>
+          </div>
       </section>
     </section>
   );
