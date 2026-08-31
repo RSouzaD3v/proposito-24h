56212821a38dcbcec1eb8adf41b7cc31074b7dd5
RsouzaD3v <rafaelf.souzan02@gmail.com>
2026-08-31 18:50:40 -0300

    [FIX] Envio de PDF

--- src/components/S3UploaderPdf.tsx
+++ src/components/S3UploaderPdf.tsx
@@ -60,6 +60,7 @@ export default function S3UploaderPdf({ folder, onUploaded }: Props) {
         className="hidden"
       />
       <button
+        type="button"
         onClick={() => fileInputRef.current?.click()}
         disabled={uploading}
         className="px-4 py-2 bg-blue-500 text-white font-medium rounded-lg shadow hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-blue-300"
